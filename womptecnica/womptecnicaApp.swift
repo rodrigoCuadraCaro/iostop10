c564c530024406f50c191283a1304e097918c887
rodrigoCuadraCaro <rodrigo.cuadra.dev@gmail.com>
2024-03-06 00:04:08 -0300

    Initial Commit

---
+++ womptecnica/womptecnicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  womptecnicaApp.swift
+//  womptecnica
+//
+//  Created by Rodrigo Cuadra on 06-03-24.
+//
+
+import SwiftUI
+
+@main
+struct womptecnicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
